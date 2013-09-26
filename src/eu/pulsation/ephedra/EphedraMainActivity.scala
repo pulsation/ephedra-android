39fb9221cb9eeb54ca6853bf09f6f03f982e9867
pulsation <philippe.sam-long@laposte.net>
2013-09-26 12:15:07 +0200

    Use promise instead of spawn {} at last

--- src/eu/pulsation/ephedra/EphedraMainActivity.scala
+++ src/eu/pulsation/ephedra/EphedraMainActivity.scala
@@ -1,7 +1,8 @@
 package eu.pulsation.ephedra
 
 import scala.collection.JavaConversions._
-import scala.concurrent.ops._
+import scala.concurrent._
+import ExecutionContext.Implicits.global
 
 import android.app.ListActivity
 import android.os.Bundle
@@ -13,52 +14,41 @@ class EphedraMainActivity extends ListActivity
 {
 
   final private val TAG="EphedraMainActivity"
-    
-  lazy val adapter = new ArrayAdapter[String](this, android.R.layout.simple_list_item_1/*, new Array()rssTitles*/)
-
   
-   implicit def toRunnable[F](f: => F): Runnable =
-new Runnable() { def run() = f }
+  // Needed to be converted in a Runnable called by runOnUiThread()
+  implicit def toRunnable[F](f: => F): Runnable = new Runnable() { def run() = f }
 
   /** Called when the activity is first created. */
   override def onCreate(savedInstanceState: Bundle)
   {
+    val adapter = new ArrayAdapter[String](this, android.R.layout.simple_list_item_1/*, new Array()rssTitles*/)
     lazy val ephedraAlarmHelper = new EphedraAlarmHelper(this)
-/*
-    lazy val rssItems = new EphedraRSSFeed(this.getResources().getString(R.string.rss_feed)).items
-    lazy val rssTitles = rssItems.map(item => item.title).toArray
-*/
 
     super.onCreate(savedInstanceState)
     setContentView(R.layout.main)
 
     // Launch periodic updates
     // ephedraAlarmHelper.startAlarm()
-    
-
-    // Log.v(TAG, rssItems.length + " RSS items loaded")
 
     this.setListAdapter(adapter)
-
-    /*
-    def populateArray(rssFeed : EphedraRSSFeed) = {
-      val items = rssFeed.items
-
-      adapter.addAll(items.map(item => item.title));
+    
+    val promise: Future[EphedraRSSFeed] = future {
+      new EphedraRSSFeed(this.getResources().getString(R.string.rss_feed))
     }
-    */
 
-    /*
-    val task = new EphedraLoadRSSAsyncTask(populateArray)
-    task.execute(this.getResources().getString(R.string.rss_feed))
-    */
+    promise onSuccess {
+      case rssFeed => {
+        runOnUiThread({
+          adapter.addAll(rssFeed.items.map(item => item.title))
+        })
+      }
+    }
 
-  }
-
-  spawn {
-    val rssFeed = new EphedraRSSFeed(this.getResources().getString(R.string.rss_feed))
-    runOnUiThread {
-      this.adapter.addAll(rssFeed.items.map(item => item.title))
+    promise onFailure {
+      case error => { 
+        // TODO: toast on error
+        Log.v(TAG, error.getMessage)
+      }
     }
   }
 
