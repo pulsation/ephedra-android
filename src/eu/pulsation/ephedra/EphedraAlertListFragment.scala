b0f403eed4c757a9f01119476d78dc3e8f2cb5a0
pulsation <philippe.sam-long@laposte.net>
2013-09-26 18:00:15 +0200

    Prepare using fragments for the alert list

---
+++ src/eu/pulsation/ephedra/EphedraAlertListFragment.scala
@@ -0,0 +1,77 @@
+package eu.pulsation.ephedra
+
+import scala.collection.JavaConversions._
+import scala.concurrent._
+import scala.language.implicitConversions
+import ExecutionContext.Implicits.global
+
+import android.app.ListFragment
+import android.os.Bundle
+import android.view.{Menu, View}
+import android.widget.{ArrayAdapter, Toast, ListView}
+import android.util.Log
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+
+class EphedraAlertListFragment extends ListFragment 
+{
+  final private val TAG="EphedraAlertListFragment"
+  
+  // Needed to be converted in a Runnable called by runOnUiThread()
+  implicit def toRunnable[F](f: => F): Runnable = new Runnable() { def run() = f }
+
+  override def onCreateView(inflater: LayoutInflater, container: ViewGroup, savedInstanceState: Bundle):View = {
+    inflater.inflate(R.layout.alert_list, container, false)
+  }
+
+  lazy val context = this.getActivity()
+
+  /** Called when the activity is first created. */
+  override def onCreate(savedInstanceState: Bundle) {
+    val adapter = new ArrayAdapter[String](context, android.R.layout.simple_list_item_1)
+    lazy val ephedraAlarmHelper = new EphedraAlarmHelper(context)
+
+    super.onCreate(savedInstanceState)
+    // setContentView(R.layout.main)
+
+    // Launch periodic updates
+    // ephedraAlarmHelper.startAlarm()
+
+    this.setListAdapter(adapter)
+    
+    val promise: Future[List[EphedraRSSItem]] = future {
+      (new EphedraRSSFeed(this.getResources().getString(R.string.rss_feed))).items
+    }
+
+    promise onSuccess {
+      case items => {
+        context.runOnUiThread({
+          adapter.addAll(items.map(item => item.title))
+        })
+      }
+    }
+
+    promise onFailure {
+      case error => {
+        Log.v(TAG, error.getMessage)
+        Toast
+          .makeText(context.getApplicationContext(), 
+            this.getString(R.string.failed_loading_feed),
+            Toast.LENGTH_LONG)
+          .show()
+      }
+    }
+  }
+
+  override def onListItemClick(lv: ListView, v: View, position: Int, id: Long) {
+    Log.v(TAG, "onItemClick - position: " + position + "; id: " + id)
+  }
+
+  /*
+  override def onCreateOptionsMenu(menu: Menu) : Boolean = {
+    this.getMenuInflater().inflate(R.menu.menu, menu)
+    true
+  }
+  */
+}
