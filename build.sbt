a2c7f65991f0bbe98b0dc91178557a961aa44100
pulsation <philippe.sam-long@laposte.net>
2013-09-26 11:49:04 +0200

    Replace AsyncTask with scala spawn {}

--- build.sbt
+++ build.sbt
@@ -5,5 +5,4 @@ android.Plugin.androidBuild
 name := "Ephedra"
 
 proguardOptions in Android ++= Seq(
-  "-keepclasseswithmembers class eu.pulsation.ephedra.EphedraLoadRSSAsyncTask { protected eu.pulsation.ephedra.EphedraRSSFeed doInBackground(String...); }"
 )
--- src/eu/pulsation/ephedra/EphedraMainActivity.scala
+++ src/eu/pulsation/ephedra/EphedraMainActivity.scala
@@ -1,6 +1,7 @@
 package eu.pulsation.ephedra
 
 import scala.collection.JavaConversions._
+import scala.concurrent.ops._
 
 import android.app.ListActivity
 import android.os.Bundle
@@ -8,11 +9,17 @@ import android.view.Menu
 import android.widget.ArrayAdapter
 import android.util.Log
 
-class EphedraMainActivity extends ListActivity
+class EphedraMainActivity extends ListActivity 
 {
 
   final private val TAG="EphedraMainActivity"
     
+  lazy val adapter = new ArrayAdapter[String](this, android.R.layout.simple_list_item_1/*, new Array()rssTitles*/)
+
+  
+   implicit def toRunnable[F](f: => F): Runnable =
+new Runnable() { def run() = f }
+
   /** Called when the activity is first created. */
   override def onCreate(savedInstanceState: Bundle)
   {
@@ -28,21 +35,31 @@ class EphedraMainActivity extends ListActivity
     // Launch periodic updates
     // ephedraAlarmHelper.startAlarm()
     
-    val adapter = new ArrayAdapter[String](this, android.R.layout.simple_list_item_1/*, new Array()rssTitles*/)
 
     // Log.v(TAG, rssItems.length + " RSS items loaded")
 
     this.setListAdapter(adapter)
 
+    /*
     def populateArray(rssFeed : EphedraRSSFeed) = {
       val items = rssFeed.items
 
       adapter.addAll(items.map(item => item.title));
     }
+    */
 
+    /*
     val task = new EphedraLoadRSSAsyncTask(populateArray)
     task.execute(this.getResources().getString(R.string.rss_feed))
+    */
 
+  }
+
+  spawn {
+    val rssFeed = new EphedraRSSFeed(this.getResources().getString(R.string.rss_feed))
+    runOnUiThread {
+      this.adapter.addAll(rssFeed.items.map(item => item.title))
+    }
   }
 
   override def onCreateOptionsMenu(menu: Menu) : Boolean = {
